30461cb7da527b784a1787859e1847129be6262c
Gustavo Tellez <126348310+gusgoos@users.noreply.github.com>
2023-11-09 14:22:07 -0600

    Add files via upload
    API y base de datos mergeada, actualizada, con recolector clara incluida, mismos endpoints que api abi

--- api.py
+++ api.py
@@ -1,97 +1,124 @@
+import os
 import pyodbc
+from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 
+load_dotenv('app.env')
+
 conn_str = (
-    "DRIVER={ODBC Driver 18 for SQL Server};"
-    "SERVER=10.14.255.85;"
-    "DATABASE=Donation_Tracking_DB;"
-    "UID=SA;"
-    "PWD=Shakira123.;"
-    "TrustServerCertificate=yes"
+    f"DRIVER={os.environ.get('DRIVER')};"
+    f"SERVER={os.environ.get('SERVER')};"
+    f"DATABASE={os.environ.get('DATABASE')};"
+    f"UID={os.environ.get('UID')};"
+    f"PWD={os.environ.get('PASS')};"
+    f"TrustServerCertificate={os.environ.get('TrustServerCertificate')}"
 )
 
 app = Flask(__name__)
 
 def get_db_connection():
+    """
+    Method to create a connection with the database.
+
+    Parameters:
+    Null
+
+    Returns:
+    The connection to the database if successful, otherwise returns an empty value.
+    """
     try:
         conn = pyodbc.connect(conn_str)
         return conn
     except Exception as e:
         return None
+    
+############### ROUTES ###################################
 
-#ROUTES
-#########################################################################
-#Login testeo de API (sin sql)
-@app.route('/login', methods=['GET'])
+@app.route('/test', methods=['GET'])
 def login():
+    """
+    Endpoint to test the operational status of the API
+
+    Parameters:
+    Null
+
+    Returns:
+    Message to indicate API is up and status 200 OK
+    """
     if True:
         return jsonify({'message': 'Funciona'}), 200
-#########################################################################
 
+@app.route('/recibosManager', methods=['GET'])
+def obtener_datos_generales():
+    """
+    Endpoint to obtain all the data from the receipts of a particular day to be monitored by the manager.
 
+    Parameters:
+    Null
 
-#########################################################################
-#Para checar que Put sirve
-@app.route('/getAllRecibos', methods=['GET'])
-def get_bitacora_pagos_donativos():
+    Returns:
+    The data from all receipts of a particular day, otherwise an error message.
+    """
     try:
         connection = pyodbc.connect(conn_str)
         cursor = connection.cursor()
-        cursor.execute("""
-            SELECT
-                OPE_BITACORA_PAGOS_DONATIVOS.ID_DONANTE,
-                OPE_BITACORA_PAGOS_DONATIVOS.IMPORTE,
-                OPE_BITACORA_PAGOS_DONATIVOS.ESTATUS_PAGO,
-                OPE_DONANTES.TEL_CASA,
-                OPE_DONANTES.TEL_MOVIL,
-                OPE_DIRECCIONES_COBRO.DIRECCION,
-                OPE_DIRECCIONES_COBRO.REFERENCIA_DOMICILIO,
-                OPE_DIRECCIONES_COBRO.COMENTARIOS,
-                OPE_RECOLECTORES.UsuarioRecolector
-            FROM OPE_BITACORA_PAGOS_DONATIVOS
-            INNER JOIN OPE_DONANTES ON OPE_BITACORA_PAGOS_DONATIVOS.ID_DONANTE = OPE_DONANTES.ID_DONANTE
-            INNER JOIN OPE_DIRECCIONES_COBRO ON OPE_DONANTES.ID_DIRECCION_COBRO = OPE_DIRECCIONES_COBRO.ID_DIRECCION_COBRO
-            INNER JOIN OPE_RECOLECTORES ON OPE_BITACORA_PAGOS_DONATIVOS.ID_RECOLECTOR = OPE_RECOLECTORES.ID_RECOLECTOR
-        """)
-        results = cursor.fetchall()
+        cursor.execute("EXEC ObtenerDatosDiariosManager")
+        data = [{'id': int(row[0]), 'ID_DONANTE': int(row[1]), 'ID_RECOLECTOR': int(row[2]), 'USUARIO_RECOLECTOR': row[3], 'ID_RECIBO': row[4], 'FECHA_COBRO': row[5], 'FECHA_PAGO': row[6], 'IMPORTE': row[7], 'ESTATUS_PAGO': row[8], 'COMENTARIOS': row[9], 'TEL_CASA': row[10], 'TEL_MOVIL': row[11], 'DIRECCION': row[12], 'REFERENCIA_DOMICILIO': row[13], 'NOMBRE_DONANTE': row[14]} for row in cursor.fetchall()]
         cursor.close()
         connection.close()
-        data = []
-        for row in results:
-            data.append({
-                "ID_DONANTE": row.ID_DONANTE,
-                "IMPORTE": row.IMPORTE,
-                "ESTATUS_PAGO": row.ESTATUS_PAGO,
-                "TEL_CASA": row.TEL_CASA,
-                "TEL_MOVIL": row.TEL_MOVIL,
-                "DIRECCION": row.DIRECCION,
-                "REFERENCIA_DOMICILIO": row.REFERENCIA_DOMICILIO,
-                "COMENTARIOS": row.COMENTARIOS,
-                "UsuarioRecolector": row.UsuarioRecolector
-            })
         return jsonify(data), 200
-
+    
     except Exception as e:
         error_message = "An error occurred while processing the request: " + str(e)
         return jsonify({'error': error_message}), 500
-#########################################################################
 
+@app.route('/recibosRecolector', methods=['POST'])
+def obtener_recibos_recolector():
+    """
+    Endpoint to obtain the data of all the receipts assigned to a particular delivery guy on a
+    particular day.
 
+    Parameters:
+    IdRecolector: Id of the Delivery Guy from which to obtain the receipts.
 
-#########################################################################
-# Envio de Datos, actualizacion por botón
-@app.route('/actualizarRecibo', methods=['PUT'])
-def actualizar_recibo_especifico():
+    Returns:
+    The data from all receipts of a particular delivery guy and a particular day, otherwise
+    an error message.
+    """
+    data = request.get_json()
+    IdRecolector = data['IdRecolector']
     try:
-        # Get JSON data from the request
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('EXEC ObtenerDatosDiarios @IdRecolector = ?', (IdRecolector,))
+        datos = [{'id': int(row[0]), 'ID_RECIBO': row[1], 'ID_DONANTE': int(row[2]), 'IMPORTE': row[3], 'ESTATUS_PAGO': row[4], 'TEL_CASA': row[5], 'TEL_MOVIL': row[6], 'DIRECCION': row[7], 'REFERENCIA_DOMICILIO': row[8], 'NOMBRE_DONANTE': row[9],} for row in cursor.fetchall()]
+        cursor.close()
+        conn.close()
+        return jsonify(datos), 200
+    except Exception as e:
+        error_message = "An error occurred while processing the request: " + str(e)
+        return jsonify({'error': error_message}), 500
+
+@app.route('/actualizarRecibo', methods=['PUT'])
+def actualizar_recibo():
+    """
+    Endpoint to update a receipt.
+
+    Parameters:
+    Null
+
+    Returns:
+    A successful update message, otherwise an error message.
+    """
+    try:
         data = request.get_json()
-        idDonativo = data['ID_DONATIVO']
+        idBitacora = data['ID_BITACORA']
         estatusPago = data['ESTATUS_PAGO']
         comentarios = data['COMENTARIOS']
 
         connection = pyodbc.connect(conn_str)
         cursor = connection.cursor()
-        cursor.execute("EXEC ActualizarBitacoraPagosDonativos @ID_DONATIVO = ?, @ESTATUS_PAGO = ?, @COMENTARIOS = ?", idDonativo, estatusPago, comentarios)
+        cursor.execute("EXEC ActualizarBitacoraPagosDonativos @ID_BITACORA = ?, @ESTATUS_PAGO = ?, @COMENTARIOS = ?", idBitacora, estatusPago, comentarios)
         connection.commit()
         cursor.close()
         connection.close()
@@ -100,79 +127,83 @@ def actualizar_recibo_especifico():
     except Exception as e:
         error_message = "An error occurred while processing the request: " + str(e)
         return jsonify({'error': error_message}), 500
-#########################################################################
+    
 
+@app.route('/loginRecolector', methods=['POST'])
+def login_recolector():
+    """
+    Endpoint to validate the credentials of a delivery guy.
 
+    Parameters:
+    USUARIO: Username of the delivery guy.
+    PASS: Password of the delivery guy.
 
-#########################################################################
-# Obtención de datos de todos los repartidores, de todas las fechas
-@app.route('/datosDiarios', methods=['GET'])
-def obtener_datos_diarios():
+    Returns:
+    The authentication status, otherwise an error message.
+    """
+    data = request.get_json()
+    username = data['USUARIO']
+    password = data['PASS']
     try:
-        connection = pyodbc.connect(conn_str)
-        cursor = connection.cursor()
-        cursor.execute("EXEC ObtenerDatosDiarios")
-        datos = [{'id': row[0], 'IMPORTE': row[1], 'ESTATUS_PAGO': row[2], 'TEL_CASA': row[3], 'TEL_MOVIL': row[4], 'DIRECCION': row[5], 'REFERENCIA_DOMICILIO': row[6]} for row in cursor.fetchall()]
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("EXEC VerificarCredencialesRecolector @USUARIO = ?, @PASS = ?", (username, password))
+        result = cursor.fetchone()
+
+        if result is not None:
+            response = {'id': int(result[0])}
+        else:
+            response = {'id': 0}
+
         cursor.close()
-        connection.close()
-        return jsonify(datos), 200
+        conn.close()
+        return jsonify(response), 200
+
     except Exception as e:
         error_message = "An error occurred while processing the request: " + str(e)
         return jsonify({'error': error_message}), 500
-#########################################################################
 
+@app.route('/loginManager', methods=['POST'])
+def login_manager():
+    """
+    Endpoint to validate the credentials of a manager.
 
+    Parameters:
+    USUARIO: Username of the manager.
+    PASS: Password of the manager.
 
-#########################################################################
-# # Obtención de datos, por usuario, por fecha actual
-# @app.route('/datosDiarios/<int:IdRecolector>', methods=['GET'])
-# def obtener_datos_diarios(IdRecolector):
-#     try:
-#         conn = get_db_connection()
-#         cursor = conn.cursor()
-#         cursor.execute('EXEC ObtenerDatosDiarios @IdRecolector = ?', (IdRecolector,))
-#         datos = [{'ID_BITACORA': row[0], 'ID_DONANTE': row[1], 'IMPORTE': row[2], 'ID_DONATIVO': row[3], 'ESTATUS_PAGO': row[4], 'TEL_CASA': row[5], 'TEL_MOVIL': row[6], 'DIRECCION': row[7], 'REFERENCIA_DOMICILIO': row[8]} for row in cursor.fetchall()]
-#         cursor.close()
-#         conn.close()
-#         return jsonify(datos), 200
-#     except Exception as e:
-#         return jsonify({'error': 'Error fetching data'}), 500
-#########################################################################
+    Returns:
+    The authentication status, otherwise an error message.
+    """
+    data = request.get_json()
+    username = data['USUARIO']
+    password = data['PASS']
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
 
+        cursor.execute("EXEC VerificarCredencialesManager @USUARIO = ?, @PASS = ?", (username, password))
+        result = cursor.fetchone()
 
+        if result is not None:
+            if result[0] == "Authentication failed":
+                response = {'message': 'Authentication failed'}
+            elif result[0] == "Authentication successful":
+                response = {'message': 'Authentication successful'}
+            else:
+                print(result[0])
+                response = {'message': 'Query Result Not Expected'}
+        else:
+            response = {'message': 'Empty Query'}
+        
+        cursor.close()
+        conn.close()
+        return jsonify(response), 200
 
-#########################################################################
-# Login bien hecho
-# @app.route('/verificarCredenciales', methods=['POST'])
-# def verify_credentials():
-#     data = request.get_json()
-#     username = data['USUARIO']
-#     password = data['PASS']
-#     try:
-#         conn = get_db_connection()
-#         cursor = conn.cursor()
+    except Exception as e:
+        error_message = "An error occurred while processing the request: " + str(e)
+        return jsonify({'error': error_message}), 500
 
-#         cursor.execute("EXEC VerificarCredencialesRecolector @USUARIO = ?, @PASS = ?", (username, password))
-#         result = cursor.fetchone()
-
-#         if result is not None:
-#             if result[0] == "Username doesnt exist":
-#                 response = {'message': 'Username doesnt exist'}
-#             elif result[0] == "Login successful":
-#                 response = {'message': 'Login successful'}
-#             elif result[0] == "Incorrect Password":
-#                 response = {'message': 'Incorrect password'}
-#             else:
-#                 response = {'message': 'Query Result Not Expected'}
-#         else:
-#             response = {'message': 'Query Empty'}
-
-#         cursor.close()
-#         conn.close()
-#         return jsonify(response), 200
-#     except Exception as e:
-#         return jsonify({'error': 'An error occurred while verifying credentials (Exception)'}), 500
-#########################################################################
 
 # Your other API endpoints
 if __name__ == '__main__':
