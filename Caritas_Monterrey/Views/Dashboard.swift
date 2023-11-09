8ca238c8e56c6e25879c923dcd771ab593606f68
Maffergu <60632469+Maffergu@users.noreply.github.com>
2023-11-09 14:20:13 -0600

    Nuevo botón

--- Caritas_Monterrey/Views/Dashboard.swift
+++ Caritas_Monterrey/Views/Dashboard.swift
@@ -10,35 +10,70 @@ import SwiftUI
 struct Dashboard: View {
     @State var listaPrueba :Array<Card> = []
     @State private var selectedFilter = 0
+    @State private var filtro = "Todos"
+    let filtros = ["No cobrados", "Cobrados", "Todos"]
     
     var body: some View {
         NavigationStack {
             VStack{
                 ZStack {
-                    Image("top")
-                    .resizable(resizingMode: .stretch)
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 435, height: 191)
-                    .clipped()
-                        
+                    Rectangle()
+                      .foregroundColor(.clear)
+                      .frame(width: 435, height: 191)
+                      .background(
+                        Image("top")
+                            .resizable(resizingMode: .stretch)
+                          .aspectRatio(contentMode: .fill)
+                          .frame(width: 435, height: 191)
+                          .clipped()
+                      )
+                    
                     VStack(alignment: .leading) {
                         Text("Bienvenido de Vuelta")
-                            .font(.system(size: 26).bold())
+                            .font(.system(size: 30).bold())
                             .fontWeight(.bold)
                             .foregroundColor(.white)
                             .padding(.trailing, 60)
+                            .padding(.top, 30)
+                        
                         Text("Estas son tus recolecciones de hoy")
-                            .font(.title3)
-                            .fontWeight(.semibold)
-                            .foregroundColor(.white)
-                            .padding(.trailing, 30)
-                        Picker(selection: $selectedFilter, label: Text("Ordenar por").font(.title)) {
-                                                Text("No cobrados").tag(0)
-                                                Text("Cobrados").tag(1)
-                        }.colorMultiply(.black).colorInvert()
-                            .padding(.top, -19)
-                            .padding(.leading, -11)
+                             .font(.system(size: 20))
+                             .fontWeight(.semibold)
+                             .foregroundColor(.white)
+                             .padding(.trailing, 30)
+                        
+                       
+                        Menu {
+                            ForEach(filtros, id: \.self) { option in
+                                Button(action: {
+                                    filtro = option
+                                }) {
+                                    HStack {
+                                        Text(option)
+                                        if option == filtro {
+                                            Image(systemName: "checkmark")
+                                        }
+                                    }
+                                }
+                            }
+                        } label: {
+                            HStack {
+                                Image(systemName: "slider.horizontal.3")
+                                    .foregroundColor(.white)
+                                    .bold()
+                                    .font(.system(size: 20))
+                                Text("Filtrar")
+                                    .font(.system(size: 16).bold())
+                                    .foregroundColor(.white)
+                                    
+                                
+                            }
+                            
+                        }.padding(.top, -5)
+                        
                     }
+                    
+                    
                 }
                 VStack {
                     List(listaPrueba) { cardItem in
