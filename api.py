ceec286b6d7b1b280884fdef2f4014e18470bd64
Gustavo Tellez <126348310+gusgoos@users.noreply.github.com>
2023-11-21 13:39:33 -0600

    API con documentación Swagger
    Documentación Swagger creada usando Flasgger, accesible en endpointhttp://10.14.255.85:8085/apidocs

--- api.py
+++ api.py
@@ -1,35 +1,81 @@
 from flask import Flask, request, jsonify
+from flasgger import Swagger
 import mssql_functions as mssql
 
 app = Flask(__name__)
+swagger = Swagger(app, template={
+    "swagger": "2.0",
+    "info": {
+        "title": "API Caritas MTY",
+        "description": "API para Aplicación Móvil Caritas MTY",
+        "version": "3.2.1",
+    },
+    "host": "http://10.14.255.85:8085",
+    "tags": [
+        {"name": "Manager", "description": "Endpoints para Manager"},
+        {"name": "Recolector", "description": "Endpoints para Recolector"},
+    ],
+})
 
-############### ROUTES ###################################
+@app.route('/health', methods=['GET'])
+def health_check():
+    """
+    Endpoint for health check of the API.
+    
+    ---
+    tags:
+      - Manager
+    responses:
+      200:
+        description: API is healthy and running
+    """
+    return 'API is healthy and running', 200
 
 @app.route('/recibosManager', methods=['GET'])
 def obtener_datos_generales():
     """
     Endpoint to obtain all the data from the receipts of a particular day to be monitored by the manager.
-
-    Parameters:
-    Null
-
-    Returns:
-    The data from all receipts of a particular day, otherwise an error message.
+    ---
+    tags:
+      - Manager
+    responses:
+      200:
+        description: The data from all receipts of a particular day
+      500:
+        description: Internal Server Error
+        content:
+          application/json:
+            example:
+              {
+                "error": "An error occurred while processing the request: <error_message>"
+              }
     """
     return mssql.obtener_datos_generales()
 
 @app.route('/recibosRecolector', methods=['POST'])
 def obtener_recibos_recolector():
     """
-    Endpoint to obtain the data of all the receipts assigned to a particular delivery guy on a
-    particular day.
-
-    Parameters:
-    IdRecolector: Id of the Delivery Guy from which to obtain the receipts.
-
-    Returns:
-    The data from all receipts of a particular delivery guy and a particular day, otherwise
-    an error message.
+    Endpoint to obtain the data of all the receipts assigned to a particular delivery guy from the day called.
+    ---
+    tags:
+      - Recolector
+    parameters:
+      - name: IdRecolector
+        in: formData
+        type: integer
+        required: true
+        description: ID of the Delivery Guy
+    responses:
+      200:
+        description: The data from all receipts of a particular delivery man and a particular day.
+      500:
+        description: Internal Server Error
+        content:
+          application/json:
+            example:
+              {
+                "error": "An error occurred while processing the request: <error_message>"
+              }
     """
     data = request.get_json()
     IdRecolector = data['IdRecolector']
@@ -39,57 +85,143 @@ def obtener_recibos_recolector():
 def actualizar_recibo():
     """
     Endpoint to update a receipt.
-
-    Parameters:
-    Null
-
-    Returns:
-    A successful update message, otherwise an error message.
+    ---
+    tags:
+      - Recolector
+    parameters:
+      - name: ID_BITACORA
+        in: formData
+        type: integer
+        required: true
+        description: ID of the receipt
+      - name: ESTATUS_PAGO
+        in: formData
+        type: integer
+        required: true
+        description: New status of the payment
+      - name: COMENTARIOS
+        in: formData
+        type: string
+        required: true
+        description: Comments for the receipt
+    responses:
+      200:
+        description: Succesful update message
+        content: Recibo actualizado exitosamente
+      500:
+        description: Internal Server Error
+        content:
+          application/json:
+            example:
+              {
+                "error": "An error occurred while processing the request: <error_message>"
+              }
     """
     data = request.get_json()
     idBitacora = data['ID_BITACORA']
     estatusPago = data['ESTATUS_PAGO']
     comentarios = data['COMENTARIOS']
-
     return mssql.actualizar_recibo(idBitacora, estatusPago, comentarios)
-
 
 @app.route('/loginRecolector', methods=['POST'])
 def login_recolector():
     """
-    Endpoint to validate the credentials of a delivery guy.
-
-    Parameters:
-    USUARIO: Username of the delivery guy.
-    PASS: Password of the delivery guy.
-
-    Returns:
-    The authentication status, otherwise an error message.
+    Endpoint to validate the credentials of a delivery man.
+    ---
+    tags:
+      - Recolector
+    parameters:
+      - name: USUARIO
+        in: formData
+        type: string
+        required: true
+        description: Username of the delivery man
+      - name: PASS
+        in: formData
+        type: string
+        required: true
+        description: Password of the delivery man
+    responses:
+      200:
+        description: Returns id of the delivery man if the authentication is correct, 0 if it's not.
+        content:
+          application/json:
+            examples:
+              authentication_success:
+                summary: Authentication Sucessful
+                value:
+                  {
+                    "id": 3
+                  }
+              authentication_fail:
+                summary: Authentication Sucessful
+                value:
+                  {
+                    "id": 0
+                  }
+      500:
+        description: Internal Server Error
+        content:
+          application/json:
+            example:
+              {
+                "error": "An error occurred while processing the request: <error_message>"
+              }
     """
     data = request.get_json()
     username = data['USUARIO']
     password = data['PASS']
-
     return mssql.login_recolector(username, password)
 
 @app.route('/loginManager', methods=['POST'])
 def login_manager():
     """
     Endpoint to validate the credentials of a manager.
-
-    Parameters:
-    USUARIO: Username of the manager.
-    PASS: Password of the manager.
-
-    Returns:
-    The authentication status, otherwise an error message.
+    ---
+    tags:
+      - Manager
+    parameters:
+      - name: USUARIO
+        in: formData
+        type: string
+        required: true
+        description: Username of the manager
+      - name: PASS
+        in: formData
+        type: string
+        required: true
+        description: Password of the manager
+    responses:
+      200:
+        description: Returns id of the delivery man if the authentication is correct, returns 0 otherwise.
+        content:
+          application/json:
+            examples:
+              authentication_success:
+                summary: Authentication Successful
+                value: 
+                  {
+                    "message": "Authentication Successful"
+                  }
+              authentication_fail:
+                summary: Authentication Failed
+                value: 
+                  {
+                    "message": "Authentication failed"
+                  }
+      500:
+        description: Internal Server Error
+        content:
+          application/json:
+            example:
+              {
+                "error": "An error occurred while processing the request: <error_message>"
+              }
     """
     data = request.get_json()
     username = data['USUARIO']
     password = data['PASS']
-
     return mssql.login_manager(username, password)
 
-# Your other API endpoints
 if __name__ == '__main__':
     app.run(debug=True, port=8085, host='0.0.0.0')
